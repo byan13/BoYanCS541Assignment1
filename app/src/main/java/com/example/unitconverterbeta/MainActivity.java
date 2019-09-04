0bd514a09ec9b3989fb130f0da23cddf21d6f114
Bo Yan <byan2@binghamton.edu>
2019-09-04 10:40:49 -0400

    Events updated

--- app/src/main/java/com/example/unitconverterbeta/MainActivity.java
+++ app/src/main/java/com/example/unitconverterbeta/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -30,28 +32,32 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+        FabSpeedDial sdFabLeft = findViewById(R.id.sdFabLeft);
+
+        FabSpeedDial sdFabRight = findViewById(R.id.sdFabRight);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_left, menu);
-        getMenuInflater().inflate(R.menu.menu_right, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        //getMenuInflater().inflate(R.menu.menu_left, menu);
+//        //getMenuInflater().inflate(R.menu.menu_right, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+////        //noinspection SimplifiableIfStatement
+////        if (id == R.id.action_settings) {
+////            return true;
+////        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
