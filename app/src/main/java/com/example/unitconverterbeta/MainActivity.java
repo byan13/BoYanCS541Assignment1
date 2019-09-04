ee0df24e31c5f702b41b4d32162252b9b02b3031
Bo Yan <byan2@binghamton.edu>
2019-09-03 23:12:44 -0400

    Final GUI

--- app/src/main/java/com/example/unitconverterbeta/MainActivity.java
+++ app/src/main/java/com/example/unitconverterbeta/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_left, menu);
-        //getMenuInflater().inflate(R.menu.menu_right, menu);
+        getMenuInflater().inflate(R.menu.menu_right, menu);
         return true;
     }
 
