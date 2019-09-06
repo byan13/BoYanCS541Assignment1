6a8f7690385336d9b891a85d44ace193d7817e62
Bo Yan <byan2@binghamton.edu>
2019-09-06 12:07:54 -0400

    First version done

--- app/src/main/java/com/example/unitconverterbeta/MainActivity.java
+++ app/src/main/java/com/example/unitconverterbeta/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import io.github.yavski.fabspeeddial.FabSpeedDial;
@@ -24,29 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         FabSpeedDial sdFabLeft = findViewById(R.id.sdFabLeft);
         final TextView from = findViewById(R.id.from);
         sdFabLeft.setMenuListener(new FabSpeedDial.MenuListener() {
-            @Override
             public boolean onPrepareMenu(NavigationMenu navigationMenu) {
                 return true;
             }
 
-            @Override
             public boolean onMenuItemSelected(MenuItem menuItem) {
-                from.setText(menuItem.getTitle().subSequence(5, menuItem.getTitle().length()));
+                from.setText(menuItem.getTitle().subSequence(4, menuItem.getTitle().length()));
                 return true;
             }
 
-            @Override
             public void onMenuClosed() {
 
             }
@@ -55,46 +45,169 @@ public class MainActivity extends AppCompatActivity {
         FabSpeedDial sdFabRight = findViewById(R.id.sdFabRight);
         final TextView to = findViewById(R.id.to);
         sdFabRight.setMenuListener(new FabSpeedDial.MenuListener() {
-            @Override
             public boolean onPrepareMenu(NavigationMenu navigationMenu) {
                 return true;
             }
 
-            @Override
             public boolean onMenuItemSelected(MenuItem menuItem) {
-                to.setText(menuItem.getTitle().subSequence(3, menuItem.getTitle().length()));
+                to.setText(menuItem.getTitle().subSequence(2, menuItem.getTitle().length()));
                 return true;
             }
 
-            @Override
             public void onMenuClosed() {
 
             }
         });
 
         ExtendedFloatingActionButton exFabConvert = findViewById(R.id.exFabConvert);
+        final TextView output = findViewById(R.id.output);
+        final EditText input = findViewById(R.id.input);
+        exFabConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputString = input.getText().toString();
+                if (inputString.isEmpty()) {
+                    output.setText("Nothing to be converted!");
+                } else {
+                    double inputNumber = Double.parseDouble(inputString);
+                    String inputUnit = from.getText().toString();
+                    String outputUnit = to.getText().toString();
+                    switch (inputUnit){
+                        case " miles":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber * 1.609344));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber * 1609.344));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber * 1760));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber * 5280));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 63360));
+                                    break;
+                            }
+                            break;
+                        case " kilometers":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber / 1.609));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber * 1000));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber * 1093.613));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber * 3280.84));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 39370.079));
+                                    break;
+                            }
+                            break;
+                        case " meters":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber / 1609.344));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber / 1000));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber * 1.094));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber * 3.281));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 39.37));
+                                    break;
+                            }
+                            break;
+                        case " yards":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber / 1760));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber / 1093.613));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber / 1.094));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber * 3));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 36));
+                                    break;
+                            }
+                            break;
+                        case " feet":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber / 5280));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber / 3280.84));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber / 3.281));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber / 3));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 12));
+                                    break;
+                            }
+                            break;
+                        case " inches":
+                            switch (outputUnit){
+                                case " miles":
+                                    output.setText(Double.toString(inputNumber / 63360));
+                                    break;
+                                case " kilometers":
+                                    output.setText(Double.toString(inputNumber / 39370.079));
+                                    break;
+                                case " meters":
+                                    output.setText(Double.toString(inputNumber / 39.37));
+                                    break;
+                                case " yards":
+                                    output.setText(Double.toString(inputNumber / 36));
+                                    break;
+                                case " feet":
+                                    output.setText(Double.toString(inputNumber / 12));
+                                    break;
+                                case " inches":
+                                    output.setText(Double.toString(inputNumber * 1));
+                                    break;
+                            }
+                            break;
+                    }
+                }
+            }
+        });
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        //getMenuInflater().inflate(R.menu.menu_left, menu);
-//        //getMenuInflater().inflate(R.menu.menu_right, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-////        //noinspection SimplifiableIfStatement
-////        if (id == R.id.action_settings) {
-////            return true;
-////        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
